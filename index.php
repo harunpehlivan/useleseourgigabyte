c67dde1bf7408e489f4304d566036e69bda1158c
HARUN PEHLİVAN <trcmnhp@hotmail.com>
2022-01-20 00:46:48 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title><?php echo "HARUN PEHLİVAN" ?></title>
+</head>
+<body>
+
+    <h3><?php echo "HARUN PEHLİVAN'a Hoşgeldiniz" ?></h3>
+    <?php
+    $color = "red";
+ECHO " CODER,DESIGNER,PUBLİSHER,DIGITAL CONTENT PRODUCER FOUNDER CEO ENTREPRENEUR RESEARCH AND DEVELOPMENT <br>";
+echo "22/07/1984 Çorum'da Doğdum Aslen Samsun'un Havza ilçesi <br>";
+EcHo "YENİ MAHALLE MİMAR SİNAN SOKAK NO:2 SULUOVA/AMASYA TURKEY<br>";
+?> 
+​<?php
+$txt = "harunpehlivan.fm.tc";
+echo "TEBİMTEBİTAGEM GAZETESİ RADYO TELEVİZYONU $txt!";
+?>
+ 
+</body>
+</html>
